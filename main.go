9c5fb5e8ed948cb66aa41768e51a505526801214
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-06-29 16:41:10 +0200

    tweaks

--- main.go
+++ main.go
@@ -6,32 +6,30 @@ import (
 )
 
 const (
-	serverPort = "8080"
+	filePathRoot = "."
+	serverPort   = "8080"
 )
 
 func main() {
 	log.Printf("starting server on port %v\n", serverPort)
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		writer.WriteHeader(200)
-		_, err := writer.Write([]byte("OK"))
-		if err != nil {
-			log.Printf("writer error: %s", err.Error())
-		}
-	})
+	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := http.Server{
 		Addr:    "localhost:" + serverPort,
 		Handler: mux,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("server error: %s\n", err.Error())
-	}
+	log.Fatal(server.ListenAndServe())
+}
 
-	log.Println("shutting down")
+func handlerReadiness(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	_, err := writer.Write([]byte("OK"))
+	if err != nil {
+		log.Printf("writer error: %s", err.Error())
+	}
 }

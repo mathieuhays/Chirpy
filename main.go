731aee1c6fee5ea75399e7f6e6dfa8fd43c0f353
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-06-29 17:26:27 +0200

    add hit counter middleware and only allow get requests

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,12 +11,40 @@ const (
 	serverPort   = "8080"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		a.fileServerHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (a *apiConfig) handlerReset(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(200)
+	a.fileServerHits = 0
+}
+
+func (a *apiConfig) handlerMetrics(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(200)
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	_, err := writer.Write([]byte(fmt.Sprintf("Hits: %v", a.fileServerHits)))
+	if err != nil {
+		log.Printf("metrics error: %s", err.Error())
+	}
+}
+
 func main() {
 	log.Printf("starting server on port %v\n", serverPort)
 
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot)))))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", cfg.handlerMetrics)
+	mux.HandleFunc("GET /reset", cfg.handlerReset)
 
 	server := http.Server{
 		Addr:    "localhost:" + serverPort,

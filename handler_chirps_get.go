2fa28947b6c7f3b2020373ccf396d919316bb09b
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-07-04 10:40:03 +0200

    chirps endpoint, add author_id filter

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"sort"
 	"strconv"
 )
 
@@ -13,16 +14,39 @@ func (a *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var output = make([]chirp, len(chirps))
-	for i, c := range chirps {
-		output[i] = chirp{
+	authorIdRaw := r.URL.Query().Get("author_id")
+	baseLen := len(chirps)
+	var authorId *int
+
+	if authorIdRaw != "" {
+		id, err := strconv.Atoi(authorIdRaw)
+		if err != nil {
+			writeError(w, errors.New("invalid parameter"), http.StatusBadRequest)
+			return
+		}
+
+		baseLen = 0 // can't predict final array size anymore
+		authorId = &id
+	}
+
+	var output = make([]chirp, baseLen)
+	for _, c := range chirps {
+		if authorId != nil && c.AuthorId != *authorId {
+			continue
+		}
+
+		output = append(output, chirp{
 			Id:       c.Id,
 			Body:     c.Body,
 			AuthorId: c.AuthorId,
-		}
+		})
 	}
 
-	writeJSON(w, 200, chirps)
+	sort.Slice(output, func(i, j int) bool {
+		return output[i].Id < output[j].Id
+	})
+
+	writeJSON(w, 200, output)
 }
 
 func (a *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
